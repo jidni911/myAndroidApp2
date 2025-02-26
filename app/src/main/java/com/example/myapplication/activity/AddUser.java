34dd5d3ceb529697f8d6a74b3033f2502055dc59
jidni911 <gidni441@gmail.com>
2025-02-26 12:50:56 +0600

    26 feb

--- app/src/main/java/com/example/myapplication/activity/AddUser.java
+++ app/src/main/java/com/example/myapplication/activity/AddUser.java
@@ -1,6 +1,5 @@
 package com.example.myapplication.activity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -10,22 +9,18 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.myapplication.R;
-import com.example.myapplication.databinding.ActivityUserBinding;
 
-public class User extends AppCompatActivity {
-
-    ActivityUserBinding binding;
+public class AddUser extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityUserBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-
-
-        binding.addButton.setOnClickListener(v -> {
-            startActivity(new Intent(this, AddUser.class));
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_add_user);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
         });
     }
 }
